6787754c7ad0c15900a106843d87447ef97d80e2
Michael Trevino <mtrevin93@gmail.com>
2021-09-01 15:02:21 -0400

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿--STEP 1:
+--SELECT * 
+--FROM Genre
+
+--STEP 2:
+--SELECT *
+--FROM Artist a
+--ORDER BY a.ArtistName;
+
+--STEP 3:
+--SELECT S.Title, a.ArtistName
+--FROM Song s
+--JOIN Artist a on s.ArtistId = a.Id
+
+SELECT * FROM SONG;
+
